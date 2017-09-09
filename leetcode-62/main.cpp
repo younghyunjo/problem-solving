e38eca3c24727c560d893f9b7adf2fcf52e3b2ea
Younghyun Jo <younghyunjo99@gmail.com>
2017-09-09 15:23:06 +0900

    https://leetcode.com/problems/unique-paths/description/

--- leetcode-62/main.cpp
+++ leetcode-62/main.cpp
@@ -8,33 +8,15 @@ using namespace std;
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        _m = m;
-        _n = n;
-        cache.assign(m, vector<int>(n, -1));
-        return _uniquePaths(0, 0);
+        vector<vector<int>> cache(m, vector<int>(n, 1));
+
+        for (int i=1; i<m; i++) {
+            for (int j=1; j<n; j++) {
+                cache[i][j] = cache[i-1][j] + cache[i][j-1];
+            }
+        }
+        return cache[m-1][n-1];
     }
-
-private:
-    int _uniquePaths(int x, int y) {
-        if (x >= _n)
-            return 0;
-        if (y >= _m)
-            return 0;
-
-        int& ret = cache[y][x];
-        if (ret != -1)
-            return ret;
-
-        if (x == _n-1 && y == _m-1)
-            return 1;
-
-        ret = _uniquePaths(x+1, y) + _uniquePaths(x, y+1) ;
-        return ret;
-    }
-
-    vector<vector<int>> cache;
-    int _m;
-    int _n;
 };
 
 int main() {
