fa6a43c8c29a371a532bab21750a3042cbb3bdc9
Younghyun Jo <younghyunjo99@gmail.com>
2017-11-19 00:02:32 +0900

    k interview 2nd

---
+++ kinterview-2nd-delete-word/cmake-build-debug/CMakeFiles/kakao_delete_word.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kakao_delete_word.dir/main.cpp.o"
+  "kakao_delete_word.pdb"
+  "kakao_delete_word"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kakao_delete_word.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
