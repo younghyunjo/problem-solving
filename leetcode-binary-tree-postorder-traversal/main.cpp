1a93f79055c34ddf9d70a20dde3cd27ce74b8447
Younghyun Jo <younghyunjo99@gmail.com>
2017-12-27 15:55:14 +0900

    https://leetcode.com/problems/binary-tree-postorder-traversal/description/

---
+++ leetcode-binary-tree-postorder-traversal/main.cpp
@@ -0,0 +1,63 @@
+//https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+#include <iostream>
+#include <vector>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+private:
+    vector<int> ret;
+    void _postorderTraversal(TreeNode *node) {
+        if (!node)
+            return;
+
+        if (node->left)
+            _postorderTraversal(node->left);
+        if (node->right)
+            _postorderTraversal(node->right);
+
+        ret.push_back(node->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        _postorderTraversal(root);
+        return ret;
+    }
+};
+
+
+int main() {
+//    TreeNode root;
+
+    TreeNode three(3);
+    TreeNode two(2);
+    TreeNode one(1);
+    one.right = &two;
+    two.left = &three;
+
+
+    Solution s;
+    vector<int> ret = s.postorderTraversal(&one);
+    for(auto r : ret)
+        cout << r << endl;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
