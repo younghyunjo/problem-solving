7c307ba98b9229032299088944dc5b2bf86d4d4b
Younghyun Jo <younghyunjo99@gmail.com>
2017-12-31 16:56:56 +0900

    https://www.hackerrank.com/challenges/big-sorting/problem

--- hackerrank-big-sorting/main.cpp
+++ hackerrank-big-sorting/main.cpp
@@ -4,68 +4,25 @@
 
 using namespace std;
 
-
-int stringCompare(string s1, string s2) {
+bool compare(string& s1, string& s2) {
     if (s1.length() < s2.length()) {
-        return -1;
+        return true;
     }
     else if (s1.length() > s2.length()) {
-        return 1;
+        return false;
     }
     else {
         for (int i=0; i<s1.length(); i++) {
             if (s1[i] < s2[i])
-                return -1;
+                return true;
             else if (s1[i] > s2[i])
-                return 1;
+                return false;
         }
     }
-    return 0;
+    return false;
 }
-
-void merge(vector<string>& arr, int l, int m, int r) {
-    vector<string> left;
-    left.assign(arr.begin()+l, arr.begin()+m+1);
-
-    vector<string> right;
-    right.assign(arr.begin()+m+1, arr.begin()+r+1);
-
-    int lIndex = 0;
-    int rIndex = 0;
-    int copyIndex = l;
-
-    while (lIndex <= left.size()-1 && rIndex <= right.size()-1) {
-        if (stringCompare(left[lIndex], right[rIndex]) < 0) {
-            arr[copyIndex] = left[lIndex];
-            copyIndex++;
-            lIndex++;
-        }
-        else {
-            arr[copyIndex] = right[rIndex];
-            copyIndex++;
-            rIndex++;
-        }
-    }
-
-    while(lIndex <= left.size()-1)
-        arr[copyIndex++] = left[lIndex++];
-
-    while(rIndex <= right.size()-1)
-        arr[copyIndex++] = right[rIndex++];
-}
-
-void mergesort(vector<string>& arr, int l, int r) {
-    if (l >= r)
-        return;
-
-    int m = (l+r)/2;
-    mergesort(arr, l, m);
-    mergesort(arr, m+1, r);
-    merge(arr, l, m, r);
-}
-
 vector <string> bigSorting(vector <string> arr) {
-    mergesort(arr, 0, arr.size()-1);
+    sort(arr.begin(), arr.end(), compare);
     return arr;
 }
 
