f6374fc34d8a1b701b650b0f305a98a3e6f7090c
Younghyun Jo <younghyunjo99@gmail.com>
2018-01-28 15:28:38 +0900

    https://leetcode.com/problems/merge-two-binary-trees/description/

--- leetcode-merge-two-binary-trees/main.cpp
+++ leetcode-merge-two-binary-trees/main.cpp
@@ -22,12 +22,9 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
-        if (t1 == NULL && t2 == NULL)
-            return NULL;
-
         if (t1 == NULL)
             return t2;
-        else if (t2 == NULL)
+        if (t2 == NULL)
             return t1;
         else {
             t1->val += t2->val;
